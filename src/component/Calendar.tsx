60582eab28ab5aadb2fd18f6d778cc4f2682aaa3
silvermok <rladmsahr92@naver.com>
2025-06-08 20:17:53 +0900

    조회 연동

--- src/component/Calendar.tsx
+++ src/component/Calendar.tsx
@@ -3,8 +3,23 @@ import dayjs from 'dayjs'
 import { useSwipeable } from 'react-swipeable'
 import PayDetailList from "./PayDetailList.tsx";
 import {SelectedDateContext} from "../context/SelectedDateContext.tsx";
+import api from "../lib/api.ts";
+
+type AccountGroupDto = {
+    id : number,
+    category : string,
+    content: string,
+    amount : number,
+    occurred_dt :string,
+}
+
+type AccountsResponse = {
+    date : string,
+    items : AccountGroupDto[]
+}
 
 const Calendar : React.FC = () => {
+    const [accountForm, setAccountForm] = useState<AccountsResponse[]>([]);
     const [currentDate, setCurrentDate] = useState(dayjs())
     const [clickDate, setClickDate] = useState(dayjs())
     const { selectedDate, setSelectedDate } = useContext(SelectedDateContext);
@@ -31,7 +46,21 @@ const Calendar : React.FC = () => {
         onSwipedLeft: goToNextMonth,
         onSwipedRight: goToPrevMonth,
         trackMouse: true, // 데스크탑에서도 드래그 동작 허용
-    })
+    });
+
+    useEffect(() => {
+        const fetch = async () => {
+            try{
+                const res =  await api.get(`/account/list/${selectedDate.format('YYYYMM')}`);
+                setAccountForm(res.data);
+            }
+            catch(error){
+                console.log(error);
+            }
+        };
+        fetch();
+    }, []);
+
 
     return (
         <div className="w-full max-w-md mx-auto p-4" {...handlers}>
@@ -48,24 +77,40 @@ const Calendar : React.FC = () => {
             </div>
 
             <div className="grid grid-cols-7 gap-2 mt-2">
-                {days.map((date, idx) => (
-                    <div
-                        key={idx}
-                        className={`aspect-square text-sm flex items-center justify-center rounded-lg
-              ${date ? 'cursor-pointer' : ''}
-              ${isSameDay(date, clickDate) ? 'bg-blue-500 text-white' : ''}
-              ${date && date.isSame(dayjs(), 'day') && !isSameDay(date, clickDate) ? 'border border-yellow-400' : ''}
-            `}
-                        onClick={() => {
-                            if(date){
-                                setClickDate(date);
-                                setSelectedDate(date);
-                            }
-                        }}
-                    >
-                        {date ? date.date() : ''}
-                    </div>
-                ))}
+                {days.map((date, idx) => {
+                    if (!date) {
+                        return <div key={idx}></div>; // 빈 칸
+                    }
+
+                    const dateKey = date.format('YYYYMMDD');
+                    const dayAmount = accountForm
+                        .filter(acc => acc.date === dateKey)
+                        .flatMap(acc => acc.items)
+                        .reduce((sum, acc) => sum + acc.amount, 0);
+
+                    return (
+                        <div
+                            key={idx}
+                            className={`aspect-square text-sm flex flex-col items-center justify-center rounded-lg
+                            ${date ? 'cursor-pointer' : ''}
+                            ${isSameDay(date, clickDate) ? 'bg-gray-300 text-white' : ''}
+                            ${date && date.isSame(dayjs(), 'day') && !isSameDay(date, clickDate) ? 'border border-yellow-400' : ''}
+                        `}
+                            onClick={() => {
+                                if (date) {
+                                    setClickDate(date);
+                                    setSelectedDate(date);
+                                }
+                            }}
+                        >
+                            <div>{date ? date.date() : ''}</div>
+                            <div className="text-[10px] text-gray-600 mt-1 min-h-[2em]">
+                                {dayAmount > 0 ? `${dayAmount.toLocaleString()}` : ''}
+                            </div>
+                        </div>
+                    )
+                    }
+                )}
             </div>
 
             <PayDetailList items={[]}></PayDetailList>
